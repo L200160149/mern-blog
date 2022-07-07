a7e051a5d26e7e97b9b4d87f46ab8a8b001403a6
L200160149 <ums.informatika@gmail.com>
2022-07-07 15:37:05 +0700

    change value from state global redux

--- src/config/redux/store.js
+++ src/config/redux/store.js
@@ -3,12 +3,27 @@
 const { createStore } = require("redux");
 
 const initialState = {
-    dataBlog: []
+    dataBlogs: [],
+    name: 'Dewaning'
 }
 
 // reducer = function yang mengirimkan sebuah value yang disebut dengan state yg akan diterima oleh store
     // state = value dari state global yg digunakan diseluruh halaman yg default valuenya initialState
 const reducer = (state = initialState, action) => {
+    // memisahkan state global berdasarkan action
+    if(action.type === 'UPDATE_DATA_BLOG') {
+        return {
+            ...state,
+            // penamaan payload dibawah ini bebas
+            dataBlogs: action.payload
+        }
+    }
+    if(action.type === 'UPDATE_NAME') {
+        return {
+            ...state,
+            name: 'Dewaning Daily'
+        }
+    }
     // pastikan reducer me-return sebuah state yg telah kita buat
     return state;
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,30 +4,43 @@ import { BlogItem } from "../../components/moleculs";
 import "./home.css";
 import { useHistory } from "react-router-dom";
 import Axios from "axios";
-import { useSelector } from "react-redux";
+// penerapan dispatch konsepnya sama seperti state lokal tapi penggunaanya/datanya secara global
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
-  // state
-  const [dataBlog, setDataBlog] = useState([]);
+  // state lokal
+  // const [dataBlog, setDataBlog] = useState([]);
 
   // menggunakan selector karena akan menggunakan functional component
-  const stateGlobal = useSelector(state => state)
-  console.log('state global: ', stateGlobal)
+  const {dataBlogs, name} = useSelector(state => state)
+  console.log('data blog global: ', dataBlogs)
+
+  // untuk pengkondisian dari store (diterapkan sebelum pemanggilan API)
+  const dispatch = useDispatch();
   // get data from API using Axios
   useEffect(() => {
+    // penerapan dispatch
+    setTimeout(() => {
+      dispatch({type: 'UPDATE_NAME'})
+    }, 3000)
+    
     Axios.get('http://localhost:4000/v1/blog/posts')
     .then(result => {
       // get array data
       const responseAPI = result.data;
       console.log('data API: ', responseAPI)
+      
+      // // state lokal
+      // setDataBlog(responseAPI.data)
 
-      setDataBlog(responseAPI.data)
+      // penerapan dispatch
+      dispatch({type: 'UPDATE_DATA_BLOG', payload: responseAPI.data})
     })
     .catch(err => {
       console.log('error: ', err)
     })
     // without [] it will inifinity loop
-  }, [])
+  }, [dispatch])
   const history = useHistory();
   return (
     <div className="home-page-wrapper">
@@ -37,11 +50,11 @@ const Home = () => {
           onClick={() => history.push("/create-blog")}
         />
       </div>
-
+    <p>{name}</p>
       <Gap height={20} />
 
       <div className="content-wrapper">
-        {dataBlog.map(blog => {
+        {dataBlogs.map(blog => {
           return <BlogItem 
           key={blog._id} 
           image={`http://localhost:4000/${blog.image}`}
